ba47f42f1e2679f566d76093c8d39bf21cf0740c
federico697 <federico697697@gmail.com>
2023-01-21 19:35:37 +0100

    scheletro creato

---
+++ resources/views/pages/comic/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('title-page', 'welcome')
+@section('main-content')
+    <h1>index page comic</h1>
+@endsection
---
+++ resources/views/pages/comic/show.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('title-page', 'welcome')
+@section('main-content')
+    <h1>show page comic</h1>
+@endsection
